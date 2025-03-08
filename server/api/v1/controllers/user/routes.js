5d2d4b0f16a4828fc4bcd88b1036ef272d8ce4dc
Shivam Tiwari <shivamtiwari.57.2020@gmail.com>
2025-03-08 11:29:48 +0530

    updates

--- server/api/v1/controllers/user/routes.js
+++ server/api/v1/controllers/user/routes.js
@@ -7,5 +7,3 @@ module.exports = Express.Router()
      .post('/loginUser', controller.userLogin)
      .put("/verifyLoginOtp", controller.verifyLoginOtp)
      .get("/validateToken", controller.validateToken)
-
-     // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2N2NiZGEyOTJjZDFmY2M5NDM2NjA4ZmQiLCJtb2JpbGVOdW1iZXIiOiI4MTcyODQ1NDg4IiwiaWF0IjoxNzQxNDEyOTY3LCJleHAiOjE3NDQwMDQ5Njd9.LCG83lv7pan5BQdXi_Y4gsUkPRI7cuGhY0q5_C616jM
