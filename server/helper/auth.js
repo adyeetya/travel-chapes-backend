73522d33d53abf43e86881414e47b2310a1cff93
Shivam Tiwari <shivamtiwari.57.2020@gmail.com>
2025-03-30 15:48:19 +0530

    updates

--- server/helper/auth.js
+++ server/helper/auth.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user");
+const adminModel = require("../models/admin")
 require("../../config/config");
 
 module.exports = {
@@ -8,22 +9,25 @@ module.exports = {
             if (!req.headers.token) {
                 return res.status(400).send({ responseCode: 400, responseMessage: 'Token required. Please provide a token.' });
             }
-    
+
             jwt.verify(req.headers.token, global.gConfig.jwtsecret, async (err, result) => {
                 if (err) {
                     return res.status(401).send({ responseCode: 401, responseMessage: 'Unauthorized' });
                 }
-    
-                const userResult = await userModel.findOne({ _id: result.userId });
+                let userResult
+                userResult = await userModel.findOne({ _id: result.userId });
                 if (!userResult) {
-                    return res.status(404).send({ responseCode: 404, responseMessage: 'User not found' });
+                    userResult = await adminModel.findOne({ _id: result.userId });
+                    if (!userResult) {
+                        return res.status(404).send({ responseCode: 404, responseMessage: 'User not found' });
+                    }
                 }
-    
+
                 // Attach user information to the request object for further use
                 req.userId = result.userId;
                 req.mobileNumber = result.mobileNumber;
                 req.userDetails = result;
-    
+
                 next(); // Proceed to the next middleware or route handler
             });
         } catch (error) {
@@ -31,6 +35,6 @@ module.exports = {
             res.status(500).send({ responseCode: 500, responseMessage: 'Internal Server Error' });
         }
     }
-    
+
 
 }
